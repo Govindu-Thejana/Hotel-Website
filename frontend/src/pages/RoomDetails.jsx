26ec2f3201e90268b01c31932e517f8216e32e40
Govindu Thejana <149306686+Govindu-Thejana@users.noreply.github.com>
2025-01-07 17:21:13 +0530

    Update RoomDetails.jsx
    Corrected the navigation to booking page by clicking Book Now button

--- frontend/src/pages/RoomDetails.jsx
+++ frontend/src/pages/RoomDetails.jsx
@@ -223,7 +223,7 @@ const RoomDetails = () => {
                         <Loader />
                     ) : (
                         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                            {rooms.slice(0, 3).map((similarRoom) => (
+                            {rooms.slice(0, 6).map((similarRoom) => (
                                 <motion.div
                                     key={similarRoom._id}
                                     whileHover={{ y: -5 }}

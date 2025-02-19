575a307339fc21058d2551270597df418121cd36
Govindu <govinduthejana@gmail.com>
2025-02-19 18:39:20 +0530

    FIx a small bug

--- frontend/src/pages/Accomadation.jsx
+++ frontend/src/pages/Accomadation.jsx
@@ -177,7 +177,7 @@ const Accommodation = () => {
                                 </button>
                             </div>
                         </div>
-                    )
+                    ))
                 )}
             </section>
             <Testimonials />
